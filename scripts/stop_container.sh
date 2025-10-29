484053c1d78a99a4c84780ba0aa9505802fecf2d
Danyob16 <deg7@student.london.ac.uk>
2025-10-29 10:32:55 +0000

    Modifying stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,17 @@
+# stop_cnontainer.sh
+#!/bin/bash
+# set -e
+
+# # Stop the running container (if any)
+# containerid=`docker ps | awk -F" " '{print $1}'`
+# docker rm -f $containerid
+
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+APP_NAME="sample-flask-app"
+
+# Stop and remove the specific app container if it exists
+if [ "$(docker ps -aq -f name=$APP_NAME)" ]; then
+    docker rm -f $(docker ps -aq -f name=$APP_NAME)
+fi
