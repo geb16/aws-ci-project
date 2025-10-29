c1c1791179f334c70e4c3bf50f8fa6b7bfce0481
Danyob16 <deg7@student.london.ac.uk>
2025-10-29 10:32:11 +0000

    Modifying start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,11 +1,23 @@
+# # start_container.sh
+# #!/bin/bash
+# set -e
+
+# # Pull the Docker image from Docker Hub
+# docker pull zawya20/simple-python-flask-app
+
+# # Run the Docker image as a container
+# docker run -d -p 5000:5000 zawya20/simple-python-flask-app   # add here if you have additional apps to run like bin bash.
+
+# # -d 
+# # -t
+
+
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull zawya20/simple-python-flask-app
+APP_NAME="sample-flask-app"
+IMAGE="zawya20/simple-python-flask-app:latest"
 
-# Run the Docker image as a container
-docker run -d -p 5000:5000 zawya20/simple-python-flask-app   # add here if you have additional apps to run like bin bash.
+docker rm -f $(docker ps -aq -f name=$APP_NAME) 2>/dev/null || true
 
-# -d 
-# -t
+docker run -d -p 5000:5000 --name $APP_NAME $IMAGE
