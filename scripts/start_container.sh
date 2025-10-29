1558e47dceaef6d1a4fb7b814e21e9c3a05039b4
Danyob16 <deg7@student.london.ac.uk>
2025-10-29 11:30:59 +0000

    Adding pull image to start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -18,6 +18,11 @@ set -e
 APP_NAME="sample-flask-app"
 IMAGE="zawya20/simple-python-flask-app:latest"
 
+# Stop and remove any existing container with the same name
 docker rm -f $(docker ps -aq -f name=$APP_NAME) 2>/dev/null || true
 
+# Pull the latest Docker image from Docker Hub
+docker pull $IMAGE
+
+# Run the new Docker image as a container
 docker run -d -p 5000:5000 --name $APP_NAME $IMAGE
